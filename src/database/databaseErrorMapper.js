c5b3614a58922a194cc7a184058afb9f4d8af32e
Amirhossein <4mirh03einhashemi@gmail.com>
2026-07-27 16:56:48 +0330

    fix database error mapper

--- src/database/databaseErrorMapper.js
+++ src/database/databaseErrorMapper.js
@@ -4,4 +4,6 @@ export default function mapDatabaseError(err) {
   if (err.code === "23505" && err.constraint === "users_email_key") {
     return new DatabaseDuplicateError("Email already exists");
   }
+
+  return err;
 }
