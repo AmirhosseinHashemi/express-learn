71bc257daa15693a7574ee8099a6f4f947fe92cc
Amirhossein <4mirh03einhashemi@gmail.com>
2026-07-27 16:56:31 +0330

    add variable to the .env

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,6 @@
 export default {
   port: Number(process.env.PORT),
+  enviroment: String(process.env.ENVIROMENT),
   dbHost: String(process.env.DB_HOST),
   dbPort: Number(process.env.DB_PORT),
   dbUser: String(process.env.DB_USER),
