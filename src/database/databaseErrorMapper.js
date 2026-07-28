7ea7b297e1e0a77d7a643983bf104584580ed0fa
Amirhossein <4mirh03einhashemi@gmail.com>
2026-07-28 21:03:14 +0330

    fix database erro mapper

--- src/database/databaseErrorMapper.js
+++ src/database/databaseErrorMapper.js
@@ -1,8 +1,13 @@
 import DatabaseDuplicateError from "../errors/DatabaseDuplicateError.js";
+import AppError from "../errors/AppError.js";
 
 export default function mapDatabaseError(err) {
   if (err.code === "23505" && err.constraint === "users_email_key") {
-    return new DatabaseDuplicateError("Email already exists");
+    return new AppError({
+      message: "duplicate email",
+      statusCode: 409,
+      errorCode: "DATABASE_DUPLICATE_ERROR",
+    });
   }
 
   return err;
