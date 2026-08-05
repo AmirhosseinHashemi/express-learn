0827f7cd529c936a0e7b0cebb2b9eae564264020
Amirhossein <4mirh03einhashemi@gmail.com>
2026-08-05 17:31:55 +0330

    add middleware for products and users route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import authRouter from "../modules/auth/auth.route.js";
 import productRouter from "../modules/product/product.routes.js";
 import userRouter from "../modules/users/user.route.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.use("/products", productRouter);
-router.use("/users", userRouter);
+router.use("/products", authMiddleware, productRouter);
+router.use("/users", authMiddleware, userRouter);
 router.use("/auth", authRouter);
 
 export default router;
