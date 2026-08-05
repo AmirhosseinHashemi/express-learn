e039fbe7cc95eaaa7942df266c9c84718de811e3
Amirhossein <4mirh03einhashemi@gmail.com>
2026-08-05 17:31:31 +0330

    implement me route

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -2,6 +2,14 @@ import { sendSuccessResponse } from "../../utils/response.js";
 import { authService } from "./auth.service.js";
 
 export const authController = {
+  async me(req, res) {
+    sendSuccessResponse(res, {
+      data: {
+        userId: req.user.id,
+      },
+    });
+  },
+
   async registerUser(req, res) {
     const createdUser = await authService.registerUser(req.body);
     const newUser = {
@@ -17,7 +25,7 @@ export const authController = {
 
   async loginUser(req, res) {
     const result = await authService.loginUser(req.body);
-    
+
     sendSuccessResponse(res, {
       message: "Successfully logedin",
       data: result,
--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express";
+import authMiddleware from "../../middlewares/auth.middleware.js";
 import asyncHandler from "../../utils/asyncHandler.js";
 import validate from "../../utils/validate.js";
 import { authController } from "./auth.controller.js";
 import { loginUserSchema, registerUserSchema } from "./auth.schema.js";
 
 const authRouter = express.Router();
+
+authRouter.get("/me", authMiddleware, asyncHandler(authController.me));
 
 authRouter.post(
   "/register",
