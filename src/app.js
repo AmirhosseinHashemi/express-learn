3a95163eeaa05210bf74b1b24cf283e1187ecdc0
Amirhossein <4mirh03einhashemi@gmail.com>
2026-08-10 16:51:37 +0330

    add cookie parser middleware

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ import express from "express";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import notFoundMiddleware from "./middlewares/notFound.middleware.js";
 import router from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(cookieParser());
 
 app.use(express.json());
 
